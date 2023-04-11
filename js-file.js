93733c2dc4b8a625f24b61f38f5a91395bd569f9
Danica <33384459+danicaad@users.noreply.github.com>
2023-04-11 14:22:23 +0800

    Modify grid generation to accept values other than a fixed value of 16

--- js-file.js
+++ js-file.js
@@ -1,7 +1,23 @@
 const div = document.querySelector('#container');
 
-for (let i = 0; i < 256; i++) {
+const n = 64;
+
+for (let i = 0; i < (n*n); i++) {
    const box = document.createElement('div');
    box.classList.add("box");
+   box.style.width = `${530/n}px`;
+   box.style.height = `${530/n}px`;
    div.appendChild(box);
 }
+
+const boxes = document.querySelectorAll('.box');
+
+boxes.forEach((box) => box.addEventListener("mouseover", () => {
+    box.classList.add("hovered");
+}));
+
+const resetBtn = document.querySelector('#reset');
+
+//resetBtn.addEventListener("click", () => {
+//    boxes.forEach((box) => box.classList.remove("hovered"));
+//});
