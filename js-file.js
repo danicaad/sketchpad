b3b99ce247ef15a6c4283fdf5f7f2189ca28a3ee
Danica <33384459+danicaad@users.noreply.github.com>
2023-04-11 15:13:48 +0800

    Prompt user to input number of squares per side on the grid

--- js-file.js
+++ js-file.js
@@ -1,23 +1,32 @@
-const div = document.querySelector('#container');
+function generateGrid(n) {
+    for (let i = 0; i < (n*n); i++) {
+        const box = document.createElement('div');
+        box.classList.add("box");
+        box.style.width = `${530/n}px`;
+        box.style.height = `${530/n}px`;
+        div.appendChild(box);
+     }
+    const boxes = document.querySelectorAll('.box');
 
-const n = 64;
-
-for (let i = 0; i < (n*n); i++) {
-   const box = document.createElement('div');
-   box.classList.add("box");
-   box.style.width = `${530/n}px`;
-   box.style.height = `${530/n}px`;
-   div.appendChild(box);
+    boxes.forEach((box) => box.addEventListener("mouseover", () => {
+        box.classList.add("hovered");
+    }));
 }
 
-const boxes = document.querySelectorAll('.box');
+const div = document.querySelector('#container');
+const input = document.querySelector('#input');
 
-boxes.forEach((box) => box.addEventListener("mouseover", () => {
-    box.classList.add("hovered");
-}));
+let n = 16;
 
-const resetBtn = document.querySelector('#reset');
+generateGrid(n);
 
-//resetBtn.addEventListener("click", () => {
-//    boxes.forEach((box) => box.classList.remove("hovered"));
-//});
+input.addEventListener("click", () => {
+    while(div.firstChild) {
+        div.removeChild(div.lastChild);
+    }
+    do{
+        n = prompt('How many number of squares per side? (default: 16, max: 100)');
+    } while (n > 100);
+
+    generateGrid(n);
+});
