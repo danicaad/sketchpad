0b6bac56f04472294e73b4cf5c31b4acbbafb7b4
Danica <33384459+danicaad@users.noreply.github.com>
2023-04-12 13:52:30 +0800

    Add new drawing modes and tools
    This update adds new drawing modes such as rainbow mode, sketch mode, and a new eraser tool.
    The default pen color is also changed to a darker shade compared to the previous pen color (From #A0C3D2 to rgb(89, 89, 89)).
    The color format is changed from hex to RGB values for easier value manipulation used in the newly added drawing modes.
    
    NEW DRAWING MODES:
    - Rainbow mode: Pen color is randomly generated color with alpha = 0.4
    - Sketch mode: Each pass of the pen on a square intensifies the color by 10%
    
    A minor update on prompt for setting grid size is added as well.
    The Cancel option now works properly, leaving the current grid unchanged.

--- js-file.js
+++ js-file.js
@@ -9,23 +9,45 @@ function generateGrid(n) {
 
     const boxes = document.querySelectorAll('.box');
 
-    boxes.forEach((box) => box.addEventListener("mousedown", () => {
-        box.classList.add("hovered");
-    }));
+    boxes.forEach((box) => box.addEventListener("mousedown", draw));
     boxes.forEach((box) => box.addEventListener("mouseover", draw));
 
-    clear.addEventListener("click", () => {
-        boxes.forEach((box) => box.classList.remove("hovered"));
+    clearBtn.addEventListener("click", () => {
+        boxes.forEach((box) => box.style.backgroundColor = "#F7F5EB");
     });
 }
     
 function draw(e) {
-    if(isMouseDown && e.type == "mouseover") {
-        e.target.classList.add("hovered");
+    if ((isMouseDown && e.type == "mouseover") || (e.type == "mousedown")) {
+        if (currentMode === "normal") {        
+            e.target.style.backgroundColor = `rgb(89, 89, 89)`;
+        } else if (currentMode === "rainbow") {       
+            let red = Math.floor(Math.random() * 255);
+            let green = Math.floor(Math.random() * 255);
+            let blue = Math.floor(Math.random() * 255);
+            e.target.style.backgroundColor = `rgb(${red},${green},${blue}, 0.4)`;
+        } else if (currentMode === "sketch") {            
+            let bgColor = e.target.style.backgroundColor;
+            let alphaIndex = bgColor.search(/0.\d/);
+            let alphaValue = parseFloat(bgColor.substr(alphaIndex, 3));
+            let newAlpha = alphaValue + 0.1;
+
+            if (newAlpha != 1) {
+                e.target.style.backgroundColor = `rgb(89, 89, 89, 0.1)`;
+                if (alphaIndex > 0) {
+                    e.target.style.backgroundColor = `rgb(89, 89, 89,${newAlpha})`;
+                }
+            }
+        } else if (currentMode === "eraser") {
+            e.target.style.backgroundColor = "#F7F5EB";     
+        }    
     }
 }
 
 let isMouseDown = false;
+const drawMode = ["normal","rainbow","sketch","eraser"];
+let currentMode = "normal";
+
 document.getElementById("container").onmousedown = () => isMouseDown = true;
 document.body.onmouseup = () => isMouseDown = false;
 document.body.addEventListener("dragstart", (event) => {
@@ -33,20 +55,44 @@ document.body.addEventListener("dragstart", (event) => {
 });
 
 const div = document.querySelector('#container');
-const input = document.querySelector('#input');
-const clear = document.querySelector('#clear');
+const inputBtn = document.querySelector('#input');
+const normalBtn = document.querySelector('#normal');
+const rainbowBtn = document.querySelector('#rainbow')
+const sketchBtn = document.querySelector('#sketch');
+const eraserBtn = document.querySelector('#eraser');
+const clearBtn = document.querySelector('#clear');
 
 let n = 16;
-
 generateGrid(n);
 
-input.addEventListener("click", () => {
-    while(div.firstChild) {
-        div.removeChild(div.lastChild);
-    }
+//Set Grid Size Button
+inputBtn.addEventListener("click", () => {
     do{
         n = prompt('How many number of squares per side? (default: 16, max: 100)');
     } while (n > 100);
 
+    if (n != null){
+        while(div.firstChild) {
+            div.removeChild(div.lastChild);
+        }
+    }
+
     generateGrid(n);
+});
+
+//Normal Mode Button
+normalBtn.addEventListener("click", () => {
+    currentMode = drawMode[0];
+});
+//Rainbow Mode Button
+rainbowBtn.addEventListener("click", () => {
+    currentMode = drawMode[1];
+});
+//Sketch Mode Button
+sketchBtn.addEventListener("click", () => {
+    currentMode = drawMode[2];
+});
+//Clear Mode Button
+eraserBtn.addEventListener("click", () => {
+    currentMode = drawMode[3];
 });
